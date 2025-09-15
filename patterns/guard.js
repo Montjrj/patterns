d4e1d7c977e992ce812fbaf8e0b87cfc39f35f36
Russell Montjoy <russellmontjoy@gmail.com>
2025-09-15 07:16:45 -0600

    guard finish

--- patterns/guard.js
+++ patterns/guard.js
@@ -19,7 +19,14 @@ export function multiply(num1, num2) {
  */
 export function vote(age) {
   // TODO
+if(typeof age !== "number" ||  isNaN(age)){
+  return "Age not valid."; 
 }
+if (age < 18){
+  return "You must be 18 or older to vote.";
+}
+return "Who would you like to vote for ?"; 
+} 
 
 /**
  * @param {string} str1
@@ -29,4 +36,9 @@ export function vote(age) {
  */
 export function concatenateStrings(str1, str2) {
   // TODO
+  if(typeof str1 !== "string" || typeof str2 !== "string"){
+    return undefined;
+  }
+  return str1 + str2;
+
 }
